87ab7f9c3363a3e73809eea92045ffd0a21b57fb
csee-mun <111641516+csee-mun@users.noreply.github.com>
2022-08-21 17:19:03 +0800

    Add files via upload

---
+++ ft_print_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csee-mun <csee-mun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 20:13:25 by csee-mun          #+#    #+#             */
+/*   Updated: 2022/08/21 11:48:34 by csee-mun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
